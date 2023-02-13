5ac8c3a10bd0b32c9542dd8e27ca9fb2932ecfcc
MarsIfeanyi <marschalice@gmail.com>
2023-02-13 23:04:21 +0100

    Initial Commit

---
+++ frontEnd/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="text-blue-600 font-normal items-start md:font-medium ">
+      FeedBack-Pally
+    </div>
+  );
+};
+
+export default Header;
