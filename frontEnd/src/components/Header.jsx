6b6c5460e26fda0d6012b3a792eac6f17a58f0a1
MarsIfeanyi <marschalice@gmail.com>
2023-02-13 23:12:33 +0100

    Change FeedBack-Pally to FeedBackPal

--- frontEnd/src/components/Header.jsx
+++ frontEnd/src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <div className="text-blue-600 font-normal items-start md:font-medium ">
-      FeedBack-Pally
+      FeedBackPal
     </div>
   );
 };
