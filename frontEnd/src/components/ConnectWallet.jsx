a2906e680deb4494eca0a62787f255c8d5baf5f3
MarsIfeanyi <marschalice@gmail.com>
2023-02-13 23:03:49 +0100

    Initial Commit

---
+++ frontEnd/src/components/ConnectWallet.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+//import { ConnectButton } from "web3uikit";
+
+const ConnectWallet = () => {
+  return <div>{/* <ConnectButton moralisAuth={false} /> */}</div>;
+};
+
+export default ConnectWallet;
