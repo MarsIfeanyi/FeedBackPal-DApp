7855ed2dcc54413c2e7268ffab97ad1d1449c3f1
MarsIfeanyi <marschalice@gmail.com>
2023-02-13 23:03:56 +0100

    Initial Commit

---
+++ frontEnd/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div
+      className="
+    mx-auto text-center  justify-center mt-32 font-normal mb-10 text-blue-600"
+    >
+      <p>Marcellus Ifeanyi &copy; Learnable22 </p>
+    </div>
+  );
+};
+
+export default Footer;
