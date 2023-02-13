12608de1b18ef9f5fa7d5d29de8552057f3fe67d
MarsIfeanyi <marschalice@gmail.com>
2023-02-13 23:04:14 +0100

    Initial Commit

---
+++ frontEnd/src/components/GiveMeFeedBack.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MyFeedBack from "./MyFeedBack";
+import GetMyFeedBack from "./GetMyFeedBack";
+
+const GiveMeFeedBack = () => {
+  return (
+    <div>
+      <MyFeedBack />
+      <GetMyFeedBack />
+    </div>
+  );
+};
+
+export default GiveMeFeedBack;
