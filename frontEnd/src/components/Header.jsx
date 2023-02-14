eac02564ffe489a7e7358956e6a9fd31f0263f25
MarsIfeanyi <marschalice@gmail.com>
2023-02-15 00:26:52 +0100

    Render  UserAth Component

--- frontEnd/src/components/Header.jsx
+++ frontEnd/src/components/Header.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="text-blue-600 font-normal items-start md:font-medium ">
-      FeedBackPal
+    <div className="text-blue-600 font-normal items-start md:font-medium text-4xl mt-10 ">
+      FeedBack❤️Pal
     </div>
   );
 };
