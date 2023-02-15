e0fafae2817689917b1e8f1851df30376e65b084
MarsIfeanyi <marschalice@gmail.com>
2023-02-15 22:40:55 +0100

    Add contractABI2 and ContractAddress2

---
+++ frontEnd/src/constants2/config.js
@@ -0,0 +1,76 @@
+export const contractAddress = "0x32c8dabc5B7DEcCC5d58639F3e3158e7a7B0faD4";
+
+export const contractABI = [
+  {
+    inputs: [],
+    name: "getFeedBack",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "address",
+            name: "sender",
+            type: "address",
+          },
+          {
+            internalType: "string",
+            name: "feedback",
+            type: "string",
+          },
+          {
+            internalType: "uint256",
+            name: "timestamp",
+            type: "uint256",
+          },
+        ],
+        internalType: "struct MyFeedBack.Feedback[]",
+        name: "",
+        type: "tuple[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_userFeedback",
+        type: "string",
+      },
+    ],
+    name: "sendFeedBack",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "userFeedback",
+    outputs: [
+      {
+        internalType: "address",
+        name: "sender",
+        type: "address",
+      },
+      {
+        internalType: "string",
+        name: "feedback",
+        type: "string",
+      },
+      {
+        internalType: "uint256",
+        name: "timestamp",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
